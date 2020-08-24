670f138f2a336badd68be161f3180aa915ac078c
vagrant <vagrant@localhost.localdomain>
2020-08-24 05:25:27 +0000

    fixed question_helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
 
   def current_question(problem,current_question)
     number = 0
-    problem.questions.each do |question|
+    problem.questions.order(id: :asc).each do |question|
     number += 1
       if question.id == current_question.id
         break
